7dee67b81652c7e67758195c948d65e8725bc8a9
Christian Sasso <csasso@usv.edu>
2022-07-08 01:12:54 -0700

    Refined Main infrastructure
    Refined Main infrastructure: now the Avatar is initially active and is disabled when the UI object is detected in the Hierarchy, because we don't want the avatar when the menu is open.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,17 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    GameObject avatar;
+    [SerializeField] GameObject avatar;
 
     private void Start()
     {
-        avatar = GameObject.Find("Avatar");
-        avatar.SetActive(false);
+        GameObject ui = GameObject.Find("UI");
+        if (ui != null)
+        {
+            // No avatar while the menu is open
+            avatar.SetActive(false);
+        }
     }
 
     public void LoadScene(int buildIndex)
     {
-        Debug.Log("Loading Level " + buildIndex);
+        Debug.Log("Loading scene with build index " + buildIndex);
         // Disable the avatar in the 0_Menu scene, otherwise activate it
         avatar.SetActive(buildIndex != 0);
         SceneManager.LoadScene(buildIndex);
